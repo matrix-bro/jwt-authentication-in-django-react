821ffe42baf31b76616d6e819e61d9f0c4c59ad3
matrix-bro <razmhz11@gmail.com>
2023-12-16 13:57:38 +0545

    Hosted react project using express server

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+app.use(express.static("project/dist"));
+app.get("*", (req, res) => {
+  return res.sendFile(path.resolve(__dirname, "project", "dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 
---
+++ frontend/project/src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <>
+      <h1>App</h1>
+      <p>This is APP. Some changes.</p>
+    </>
+  );
+};
+
+export default App;
