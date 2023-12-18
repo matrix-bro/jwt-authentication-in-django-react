8cc690847e5aa4911fccb1270268ee64c1d4766f
matrix-bro <razmhz11@gmail.com>
2023-12-18 11:50:25 +0545

    Fixed Redux toolkit setup and displayed dynamic links in navbar

--- frontend/project/src/components/Navbar.tsx
+++ frontend/project/src/components/Navbar.tsx
@@ -1,6 +1,9 @@
 import { Link, NavLink } from "react-router-dom";
+import { useAppSelector } from "../hooks";
 
 const Navbar = () => {
+  const { isAuthenticated } = useAppSelector((state) => state.auth);
+
   return (
     <>
       <nav className="flex justify-between items-center border border-gray-200 py-6 px-6 bg-gray-100">
@@ -10,27 +13,39 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="space-x-6">
-          <NavLink to="/" className="text-lg font-semibold hover:text-gray-500">
-            Home
-          </NavLink>
-          <NavLink
-            to="/dashboard"
-            className="text-lg font-semibold hover:text-gray-500"
-          >
-            Dashboard
-          </NavLink>
-          <NavLink
-            to="/login"
-            className="text-lg font-semibold px-6 py-3 bg-blue-500 text-white hover:bg-blue-700"
-          >
-            Login
-          </NavLink>
-          <NavLink
-            to="/register"
-            className="text-lg font-semibold px-6 py-3 bg-teal-500 text-white hover:bg-teal-700"
-          >
-            Register
-          </NavLink>
+          {isAuthenticated && (
+            <>
+              <NavLink
+                to="/"
+                className="text-lg font-semibold hover:text-gray-500"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/dashboard"
+                className="text-lg font-semibold hover:text-gray-500"
+              >
+                Dashboard
+              </NavLink>
+            </>
+          )}
+
+          {!isAuthenticated && (
+            <>
+              <NavLink
+                to="/login"
+                className="text-lg font-semibold px-6 py-3 bg-blue-500 text-white hover:bg-blue-700"
+              >
+                Login
+              </NavLink>
+              <NavLink
+                to="/register"
+                className="text-lg font-semibold px-6 py-3 bg-teal-500 text-white hover:bg-teal-700"
+              >
+                Register
+              </NavLink>
+            </>
+          )}
         </div>
       </nav>
     </>
--- frontend/project/src/main.tsx
+++ frontend/project/src/main.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { HelmetProvider } from "react-helmet-async";
+import { Provider } from "react-redux";
+import { store } from "./store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <Provider store={store}>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </Provider>
   </React.StrictMode>
 );
