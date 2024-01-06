313d2aa69d92e8b43822232c7547ed69fc1b5d0a
matrix-bro <razmhz11@gmail.com>
2024-01-06 12:51:04 +0545

    Fixed a bug in user account creation

--- backend/app/services/account_services.py
+++ backend/app/services/account_services.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 def create_user_account(first_name, last_name, email, password):
-    user = User.objects.create_user(first_name, last_name, email, password)
+    user = User.objects.create_user(first_name=first_name, last_name=last_name, email=email, password=password)
 
     # TODO: Send Email Func
 
